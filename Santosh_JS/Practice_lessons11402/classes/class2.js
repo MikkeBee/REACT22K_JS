aa21dfaba7ef09b8573189c62d77ef52763ef227
MikkeBee <mjblakley87@gmail.com>
2022-02-14 15:19:01 +0200

    add today's lessons and tasks

---
+++ Santosh_JS/Practice_lessons11402/classes/class2.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+Define a class (empty for now) that represents a recipe. Then create a new variable called recipe and
+assign it to a new instance of that class.
+**/
+
+class Recipe {}
+
+let recipe = new Recipe();
+console.log(recipe);
