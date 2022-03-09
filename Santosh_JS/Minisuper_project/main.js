0b060bf6f2da7d5f034cf40df8a5a9700b01b164
MikkeBee <mjblakley87@gmail.com>
2022-03-09 10:03:43 +0200

    add one of the final versions of the list application

--- Santosh_JS/Minisuper_project/main.js
+++ Santosh_JS/Minisuper_project/main.js
@@ -1,16 +1,28 @@
 let inputItem = document.querySelector(".inputBox");
 const button = document.querySelector(".inputSubmit");
 const itemList = document.querySelector(".listItems");
+const delButton = document.querySelector(".listItem button");
 
 const addItem = () => {
   console.log(inputItem.value);
   const inputValue = itemList.insertAdjacentHTML(
     "beforeend",
-    `<li><i class="fa-solid fa-angle-right"></i>${inputItem.value}</li>`
+    `<li class="listItem"><i class="fa-solid fa-angle-right"></i>${inputItem.value}<button>Remove item?</button></li>`
   );
+  const items = document.querySelectorAll(".listItem");
+  for (var index = 0; index < items.length; index++) {
+    items[index].querySelector("button").addEventListener("click", function () {
+      this.closest(".listItem").remove();
+    });
+  }
   inputItem.value = "";
   return inputValue;
 };
+
+//  const deleteStuff = () => {
+//    console.log("hey");
+//    this.closest(".listItem").remove();
+//  };
 
 button.addEventListener("click", addItem);
 button.addEventListener("keyup", addItem);
